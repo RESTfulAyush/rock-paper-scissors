cba4f271141d55127b4389cd3162d08ddbd9169d
ayushpanwar <ayushpanwar132003@gmail.com>
2025-05-19 11:15:34 +0530

    rock -> stone

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders Rock Paper Scissors heading", () => {
   render(<App />);
-  const heading = screen.getByText(/rock paper scissors/i);
+  const heading = screen.getByText(/stone paper scissors/i);
   expect(heading).toBeInTheDocument();
 });
